cb78dc6e0479b7b5df350769389cbf63b70e69f4
Jayne Pietraszkiewicz <Jayne@192-168-1-185.tpgi.com.au>
2017-04-25 13:01:10 +1000

    First check in

---
+++ Trip Gallery/Trip Gallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trip Gallery
+//
+//  Created by Jayne Pietraszkiewicz on 25/4/17.
+//  Copyright © 2017 Deakin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
